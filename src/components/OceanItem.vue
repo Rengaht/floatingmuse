f9fdf7b7d26f579816cf2e0b59422503cef19f7c
RengTsai <reng.tsai@mmlab.tw>
2020-06-18 12:00:28 +0800

    vue package

---
+++ src/components/OceanItem.vue
@@ -0,0 +1,15 @@
+<template>
+ <button>{{ocean.locationName}}</button>
+</template>
+
+<script>
+export default {
+  name: 'ocean-item',
+  props: ['ocean']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
